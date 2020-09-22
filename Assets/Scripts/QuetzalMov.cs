41ccb32e8fd75c647f801771481bafd05d914b77
dzHarzard45 <dzharzard96@gmail.com>
2020-09-21 23:07:08 -0500

    Comienzo del repo para QuetzalHell

---
+++ Assets/Scripts/QuetzalMov.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+public class QuetzalMov : MonoBehaviour
+{
+    private Transform playerQuetzal;
+    private Rigidbody m_Rb;
+    private Vector3 moveInput;
+    private Vector3 moveVelocity;
+   
+    [SerializeField] float speed = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Rb = GetComponent<Rigidbody>();
+        playerQuetzal = this.transform;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        moveInput = new Vector3(h, 0, v);
+        m_Rb.velocity = moveInput * speed;
+
+
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            int dir = Input.GetKeyDown(KeyCode.Space) ? -1 : 1;
+            QuickSpin(dir);
+        }
+      
+        HorizontalLean(playerQuetzal, v, 5, 1f);
+        VerticalLean(playerQuetzal, h, 20, 1f);
+    }
+
+   
+
+    void QuickSpin(int dir)
+    {
+        playerQuetzal.DOLocalRotate(new Vector3(playerQuetzal.localEulerAngles.x, playerQuetzal.localEulerAngles.y, 360 * -dir), .4f, RotateMode.LocalAxisAdd).SetEase(Ease.OutSine);
+
+    }
+
+    void HorizontalLean(Transform target, float axis, float leanLimit, float lerpTime )
+    {
+        Vector3 targetEulerAngles = target.localEulerAngles;
+        target.localEulerAngles = new Vector3(targetEulerAngles.x, targetEulerAngles.y, Mathf.LerpAngle(targetEulerAngles.z, -axis * leanLimit, lerpTime)); //Learps Horizontally creating the effect of semi-rotation when you move, like a bird.
+
+    }
+
+    void VerticalLean(Transform target, float axis, float leanLimit, float lerpTime)
+    {
+        Vector3 targetEulerAngles = target.localEulerAngles;
+        target.localEulerAngles = new Vector3(targetEulerAngles.z, targetEulerAngles.y, Mathf.LerpAngle(targetEulerAngles.z, axis * leanLimit, lerpTime));//Learps Vertically creating the effect of semi-rotation when you move, like a bird. To break and accelerate. 
+    }
+     //Lean = Inclinacion. 
+    
+}
---
+++ Assets/Scripts/RadioBulletController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RadioBulletController : MonoBehaviour
+{
+    [Header("Project Settings")]
+    [SerializeField] int numberOfProjectiles;
+    [SerializeField] float projectilesSpeed;
+    [SerializeField] GameObject projectilePrefab;
+    [SerializeField] GameObject startPoint;
+    [SerializeField] float rotationSpeed = 45f;
+    
+    
+    [Header("Private Variables")]
+    private const float radius = 1f;
+    private Vector3 startPosition;
+    private Vector3 dir = Vector3.zero;
+  
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            startPosition = startPoint.transform.position;
+            SpawnPorjectile(numberOfProjectiles);
+        }
+    }
+
+    private void SpawnPorjectile(int _numberOfProjectiles)
+    {
+        float angleStep = 360f / _numberOfProjectiles;
+        float angle = 0f;
+        projectilePrefab.transform.rotation = Quaternion.LookRotation(dir);
+
+        
+
+        for(int i =0; i <= _numberOfProjectiles -1; i++)
+        {
+            float projectileDirXPos = startPosition.x + Mathf.Sin((angle* Mathf.PI) /180) * radius;
+            float projectileDirYPos = startPosition.y + Mathf.Cos((angle * Mathf.PI) / 180) * radius;
+
+            Vector3 projectileVector = new Vector3(projectileDirXPos, projectileDirYPos, 0);
+            Vector3 projectileMoveDirection = (projectileVector - startPosition).normalized * projectilesSpeed;
+            if (dir != Vector3.zero)
+            {
+                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(dir), Time.deltaTime * rotationSpeed);
+            }
+
+            GameObject tempObj = Instantiate(projectilePrefab,startPosition, transform.rotation );
+            tempObj.GetComponent<Rigidbody>().velocity = new Vector3(projectileMoveDirection.x, 0, projectileMoveDirection.y);
+            
+            Destroy(tempObj, 10f);
+            angle += angleStep;
+        }
+    }
+}
