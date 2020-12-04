cf7d936279e53efd62dcea086988a45a6bc566af
CrimsonHao <arq.losorno@gmail.com>
2020-12-04 11:38:14 -0600

    Main Menu terminado, ui y scripting

--- Assets/Scripts/Menu/MainMenu.cs
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -2,9 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] CanvasGroup mainMenu = default;
+    [SerializeField] CanvasGroup controlsMenu = default;
+    [SerializeField] CanvasGroup creditsMenu = default;
+
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
@@ -15,5 +20,36 @@ public class MainMenu : MonoBehaviour
         Application.Quit();
     }
 
-   
+    public void GoToControls()
+    {
+        ShowView(controlsMenu);
+        HideView(mainMenu);
+    }
+
+    public void GoToMainMenu()
+    {
+        ShowView(mainMenu);
+        HideView(controlsMenu);
+        HideView(creditsMenu);
+    }
+
+    public void GoToCredits()
+    {
+        ShowView(creditsMenu);
+        HideView(mainMenu);
+    }
+
+    private void ShowView(CanvasGroup group)
+    {
+        group.alpha = 1f;
+        group.interactable = true;
+        group.blocksRaycasts = true;
+    }
+
+    private void HideView(CanvasGroup group)
+    {
+        group.alpha = 0f;
+        group.interactable = false;
+        group.blocksRaycasts = false;
+    }
 }
