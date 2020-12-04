6035aa10be56180fa521ffd87eb646d54f148530
CrimsonHao <arq.losorno@gmail.com>
2020-12-04 09:39:38 -0600

    script de vida y ui de jugador

--- Assets/Scripts/HealthPlayer.cs
+++ Assets/Scripts/HealthPlayer.cs
@@ -2,30 +2,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HealthPlayer : MonoBehaviour, IDamageable
 {
     [SerializeField] int healthPoints;
+    [SerializeField] Image healthBar;
+
     public event Action<int> onDamageTaken;
     private int currentHP;
+
+    void Start()
+    {
+        currentHP = healthPoints;
+        onDamageTaken?.Invoke(currentHP);
+    }
 
     public void Damage(int amount)
     {
         healthPoints -= amount;
         onDamageTaken?.Invoke(currentHP);
 
-        if(healthPoints <= 0)
+        healthBar.fillAmount = (currentHP / healthPoints);
+
+        if (healthPoints <= 0)
         {
             currentHP = 0;
             Destroy(this.gameObject);
             GameManager.Instance.ShowGameOver();
         }
         onDamageTaken?.Invoke(currentHP);
-    }
-
-    void Start()
-    {
-        currentHP = healthPoints;
-        onDamageTaken?.Invoke(currentHP);
-    }  
+    }    
 }
